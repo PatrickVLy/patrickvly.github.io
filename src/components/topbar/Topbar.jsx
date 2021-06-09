3cd5260883004816b5e2bbaafff30647c50d091a
Patrick Ly <patrickly72@gmail.com>
2021-06-09 16:05:00 -0700

    included icons within anchor tags

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -8,15 +8,20 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
                 <div className="left">
                 <h1>Patrick Ly</h1>
                     <div className="itemContainer">
+                    <a href="tel:408-398-8404">
                     <Person className="icon links"/>
-                    <a href="tel:408-398-8404"><span>+1 408 398 8404</span></a>
+                    <span>+1 408 398 8404</span></a>
+                    <a href = "mailto: patrickly72@gmail.com">
                     <Mail className="icon links"/>
-                    <a href = "mailto: patrickly72@gmail.com"><span>patrickly72@gmail.com</span></a>
-                    <LinkedIn className="icon links"/><a href="https://www.linkedin.com/in/patrick-ly-aa280717a/" rel="noreferrer" target="_blank"><span>LinkedIn</span></a>
+                    <span>patrickly72@gmail.com</span></a>
+                    <a href="https://www.linkedin.com/in/patrick-ly-aa280717a/" rel="noreferrer" target="_blank">
+                    <LinkedIn className="icon links"/><span>LinkedIn</span></a>
+                    <a href = "https://patrickvly.github.io/resume.pdf" rel="noreferrer" target="_blank">
                     <ListAlt className="icon links"/>
-                    <a href = "https://patrickvly.github.io/resume.pdf" rel="noreferrer" target="_blank"><span>Resume</span></a>
+                    <span>Resume</span></a>
+                    <a href = "https://www.github.com/patrickvly" rel="noreferrer" target="_blank">
                     <GitHub className="icon links"/>
-                    <a href = "https://www.github.com/patrickvly" rel="noreferrer" target="_blank"><span>GitHub</span></a>
+                    <span>GitHub</span></a>
                     </div>
                 
                 </div>
